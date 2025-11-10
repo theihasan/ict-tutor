a4d708f13baf1a5db776f2bde16c8f5bedd78c7c
Abul Hassan <imabulhasan99@gmail.com>
2025-11-11 01:33:56 +0600

    Add Filament resource for FAQs

---
+++ app/Filament/Resources/Faqs/Schemas/FaqForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Faqs\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class FaqForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('question')
+                    ->required(),
+                Textarea::make('answer')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('category')
+                    ->required(),
+                TextInput::make('sort_order')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Toggle::make('is_active')
+                    ->required(),
+            ]);
+    }
+}
